f7f135e45a2ee02d96d5e33db4c236c2d7b248d4
Diana Regalbuto <regalbuto.d@husky.neu.edu>
2018-03-01 14:50:53 -0500

    bottom nav labels

--- app/src/main/java/northeastern/is4300/pettasktracker/UsersActivity.java
+++ app/src/main/java/northeastern/is4300/pettasktracker/UsersActivity.java
@@ -20,11 +20,11 @@ public class UsersActivity extends AppCompatActivity {
                 case R.id.navigation_home:
                     mTextMessage.setText(R.string.title_home);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_pets:
+                    mTextMessage.setText(R.string.title_activity_pets);
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_users:
+                    mTextMessage.setText(R.string.title_activity_users);
                     return true;
             }
             return false;
