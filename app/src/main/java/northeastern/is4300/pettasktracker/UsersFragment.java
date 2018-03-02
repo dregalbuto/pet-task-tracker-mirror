990704e6e1bf5edf5d0e6c0dd1aa4280e3549461
Diana Regalbuto <regalbuto.d@husky.neu.edu>
2018-03-01 20:10:51 -0500

    nav works

---
+++ app/src/main/java/northeastern/is4300/pettasktracker/UsersFragment.java
@@ -0,0 +1,27 @@
+package northeastern.is4300.pettasktracker;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class UsersFragment extends Fragment {
+
+    public static UsersFragment newInstance() {
+        UsersFragment fragment = new UsersFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_users, container, false);
+    }
+
+}
