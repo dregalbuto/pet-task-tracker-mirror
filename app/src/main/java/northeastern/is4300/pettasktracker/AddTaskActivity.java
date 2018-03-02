cc54bcbea0e83cb19ca3b04162073be40a4a925e
Diana Regalbuto <regalbuto.d@husky.neu.edu>
2018-03-01 22:39:43 -0500

    add task to pet

--- app/src/main/java/northeastern/is4300/pettasktracker/AddTaskActivity.java
+++ app/src/main/java/northeastern/is4300/pettasktracker/AddTaskActivity.java
@@ -1,7 +1,12 @@
 package northeastern.is4300.pettasktracker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
 public class AddTaskActivity extends AppCompatActivity {
 
@@ -9,5 +14,45 @@ public class AddTaskActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
+
+        /* Set up drop-down menus (spinners) */
+        Spinner spinner1 = (Spinner) findViewById(R.id.spinner_task_type);
+        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this,
+                R.array.array_task_type, android.R.layout.simple_spinner_item);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner1.setAdapter(adapter1);
+
+        Spinner spinner2 = (Spinner) findViewById(R.id.spinner_task_user);
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,
+                R.array.array_task_user, android.R.layout.simple_spinner_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner2.setAdapter(adapter2);
+
+        Spinner spinner3 = (Spinner) findViewById(R.id.spinner_task_pet);
+        ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource(this,
+                R.array.array_task_pet, android.R.layout.simple_spinner_item);
+        adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner3.setAdapter(adapter3);
+
+        Spinner spinner4 = (Spinner) findViewById(R.id.spinner_task_time);
+        ArrayAdapter<CharSequence> adapter4 = ArrayAdapter.createFromResource(this,
+                R.array.array_task_time, android.R.layout.simple_spinner_item);
+        adapter4.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner4.setAdapter(adapter4);
+
+        Spinner spinner5 = (Spinner) findViewById(R.id.spinner_task_repeat);
+        ArrayAdapter<CharSequence> adapter5 = ArrayAdapter.createFromResource(this,
+                R.array.array_task_repeat, android.R.layout.simple_spinner_item);
+        adapter5.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner5.setAdapter(adapter5);
+
+        /* Set up confirmation button */
+        Button confirmationButton = (Button) findViewById(R.id.button_task_confirm);
+        confirmationButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent myIntent = new Intent(AddTaskActivity.this, MainActivity.class);
+                startActivity(myIntent);
+            }
+        });
     }
 }
--- app/src/main/java/northeastern/is4300/pettasktracker/ViewPetActivity.java
+++ app/src/main/java/northeastern/is4300/pettasktracker/ViewPetActivity.java
@@ -1,7 +1,10 @@
 package northeastern.is4300.pettasktracker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ViewPetActivity extends AppCompatActivity {
 
@@ -11,5 +14,14 @@ public class ViewPetActivity extends AppCompatActivity {
         setContentView(R.layout.activity_view_pet);
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+         /* Set up add task button */
+        Button addTaskButton = (Button) findViewById(R.id.button_pet_add_task);
+        addTaskButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent myIntent = new Intent(ViewPetActivity.this, AddTaskActivity.class);
+                startActivity(myIntent);
+            }
+        });
     }
 }
