b7bf9a8b57320466a70ee57f1fee73f18ce2ffb7
Diana Regalbuto <regalbuto.d@husky.neu.edu>
2018-03-21 23:59:55 -0400

    nav bar highlights correct item on back arrow press

--- app/src/main/java/northeastern/is4300/pettasktracker/MainActivity.java
+++ app/src/main/java/northeastern/is4300/pettasktracker/MainActivity.java
@@ -24,18 +24,26 @@ public class MainActivity extends AppCompatActivity {
         if(last_fragment != null) {
             switch (last_fragment) {
                 case VAL_FRAG_PETS:
+                    BottomNavigationView nav = (BottomNavigationView) findViewById(R.id.navigation);
+                    nav.setSelectedItemId(R.id.navigation_pets);
                     FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
                     transaction.replace(R.id.frame_layout, PetsFragment.newInstance());
                     transaction.commit();
                     break;
                 case VAL_FRAG_USERS:
+                    BottomNavigationView nav2 = (BottomNavigationView) findViewById(R.id.navigation);
+                    nav2.setSelectedItemId(R.id.navigation_users);
                     FragmentTransaction transaction2 = getSupportFragmentManager().beginTransaction();
                     transaction2.replace(R.id.frame_layout, UsersFragment.newInstance());
                     transaction2.commit();
+                    break;
                 case VAL_FRAG_TIMELINE:
+                    BottomNavigationView nav3 = (BottomNavigationView) findViewById(R.id.navigation);
+                    nav3.setSelectedItemId(R.id.navigation_home);
                     FragmentTransaction transaction3 = getSupportFragmentManager().beginTransaction();
                     transaction3.replace(R.id.frame_layout, TimelineFragment.newInstance());
                     transaction3.commit();
+                    break;
             }
         } else {
             FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
