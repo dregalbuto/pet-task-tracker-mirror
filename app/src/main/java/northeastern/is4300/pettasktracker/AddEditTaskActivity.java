4d6e28d5b68f5ed4f92866da224dc116019a7200
Diana Regalbuto <regalbuto.d@husky.neu.edu>
2018-03-22 16:18:56 -0400

    add app/app.iml to gitignore

--- app/src/main/java/northeastern/is4300/pettasktracker/AddEditTaskActivity.java
+++ app/src/main/java/northeastern/is4300/pettasktracker/AddEditTaskActivity.java
@@ -6,7 +6,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Spinner;
 
 import northeastern.is4300.pettasktracker.data.PetRepository;
@@ -14,11 +13,12 @@ import northeastern.is4300.pettasktracker.data.Task;
 import northeastern.is4300.pettasktracker.data.TaskRepository;
 import northeastern.is4300.pettasktracker.data.UserRepository;
 
-public class EditTaskActivity extends AppCompatActivity {
+public class AddTaskActivity extends AppCompatActivity {
 
     private TaskRepository taskRepository;
     private PetRepository petRepository;
     private UserRepository userRepository;
+    private long taskId;
 
     private void initRepositories() {
         taskRepository = new TaskRepository(this);
@@ -36,38 +36,60 @@ public class EditTaskActivity extends AppCompatActivity {
 
         initRepositories();
 
+        Bundle b = getIntent().getExtras();
+        if (b != null) {
+            taskId = b.getLong(GlobalVariables.KEY_TASK_ID);
+        } else {
+            taskId = -1;
+        }
+
+        Task sourceTask = taskRepository.getTaskById(taskId);
+
         /* Set up drop-down menus (spinners) */
         Spinner spinner1 = (Spinner) findViewById(R.id.spinner_task_type);
         ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this,
                 R.array.array_task_type, android.R.layout.simple_spinner_item);
         adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner1.setAdapter(adapter1);
+        if (sourceTask != null) {
+            spinner1.setSelection(adapter1.getPosition(sourceTask.getType()));
+        }
 
-        // TODO populate from petList
         Spinner spinner2 = (Spinner) findViewById(R.id.spinner_task_user);
-        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,
-                R.array.array_task_user, android.R.layout.simple_spinner_item);
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_dropdown_item, userRepository.getUserNamesList());
         adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner2.setAdapter(adapter2);
+        if (sourceTask != null) {
+            spinner2.setSelection(adapter2.getPosition(userRepository.getUserById(sourceTask.getUserId()).getName()));
+        }
 
-        // TODO populate from userList
         Spinner spinner3 = (Spinner) findViewById(R.id.spinner_task_pet);
-        ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource(this,
-                R.array.array_task_pet, android.R.layout.simple_spinner_item);
+        ArrayAdapter<String> adapter3 = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, petRepository.getPetNamesList());
         adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner3.setAdapter(adapter3);
+        if (sourceTask != null) {
+            spinner3.setSelection(adapter3.getPosition(petRepository.getPetById(sourceTask.getPetId()).getName()));
+        }
 
         Spinner spinner4 = (Spinner) findViewById(R.id.spinner_task_time);
         ArrayAdapter<CharSequence> adapter4 = ArrayAdapter.createFromResource(this,
                 R.array.array_task_time, android.R.layout.simple_spinner_item);
         adapter4.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner4.setAdapter(adapter4);
+        if (sourceTask != null) {
+            spinner4.setSelection(adapter4.getPosition(sourceTask.getTaskTime()));
+        }
 
         Spinner spinner5 = (Spinner) findViewById(R.id.spinner_task_repeat);
         ArrayAdapter<CharSequence> adapter5 = ArrayAdapter.createFromResource(this,
                 R.array.array_task_repeat, android.R.layout.simple_spinner_item);
         adapter5.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner5.setAdapter(adapter5);
+        if (sourceTask != null) {
+            spinner5.setSelection(adapter5.getPosition(sourceTask.getRepeat()));
+        }
 
         /* Set up confirmation button */
         final Button confirmationButton = (Button) findViewById(R.id.button_task_confirm);
@@ -93,11 +115,16 @@ public class EditTaskActivity extends AppCompatActivity {
                 long userId = userRepository.getUserByName(user).getId();
                 task.setUserId(userId);
 
-                taskRepository.insertAndSetId(task);
+                if (taskId > 0) {
+                    taskRepository.updateTask(taskId, task);
+                }
+                else {
+                    taskRepository.insertAndSetId(task);
+                }
 
                 confirmationButton.setText("Success!");
 
-                Intent myIntent = new Intent(EditTaskActivity.this, MainActivity.class);
+                Intent myIntent = new Intent(AddTaskActivity.this, MainActivity.class);
                 startActivity(myIntent);
             }
         });
---
+++ app/src/main/java/northeastern/is4300/pettasktracker/GlobalVariables.java
@@ -0,0 +1,8 @@
+package northeastern.is4300.pettasktracker;
+
+/**
+ * Created by Peter on 3/22/18.
+ */
+
+public class GlobalVariables {
+}
